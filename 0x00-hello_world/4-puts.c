ac090db854ffffbbe76749e67f8d8eda7eed1c08
xymdia <miteldream@gmail.com>
2023-03-17 03:33:07 -0700

    My hello world program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * We will use main as initializer
+ * And we return 0 at program ending
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Write a C program
+ *
+ * Use the function printf
+ *
+ * Returns 0 on success
+ *
+ * Compile using -wall gcc
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,";
+	return 0;
+}
