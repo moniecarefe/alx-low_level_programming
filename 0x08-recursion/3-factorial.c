11bf22bd39e4f3ba65dab86dbb8c76dac8d39666
Xymdia <miteldream@gmail.com>
2023-04-06 08:56:25 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,18 +10,18 @@ int factorial(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		return 1;
+		return (1);
 	}
 	else if (n == 1)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 	}
 }
