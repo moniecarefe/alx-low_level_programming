ed9c3b6ac75c6c68b3ea33cf79cfaa36c0321b03
Xymdia <miteldream@gmail.com>
2023-04-06 09:36:00 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,32 @@
 #include "main.h"
+
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be
- * considered identical, otherwise return 0.
+ * wildcmp - compares two strings
  * @s1: first string
- * @s2: second string that can contain the wildcard character *
- * Return: 1 if strings can be considered identical, otherwise 0
+ * @s2: second string, can contain wildcard *
+ *
+ * Return: 1 if strings can be considered identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0') /* end of strings */
-		return (1);
 	if (*s2 == '*')
 	{
-		if (*s1 == '\0' && *(s2 + 1) == '\0') /* s2 ends with * */
-			return (1);
-		if (*s1 == '\0' && *(s2 + 1) == '*') /* s1 and s2 have consecutive * */
+		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
-		if (*s1 != '\0' && *(s2 + 1) == '\0') /* s2 ends after * */
-			return (1);
-		if (*s1 != '\0' && *(s2 + 1) == '*') /* s2 has multiple * */
-			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-		return (wildcmp(s1 + 1, s2)); /* s2 has a single * */
+		else if (!*(s1))
+			return (wildcmp(s1, s2 + 1));
+		else if (wildcmp(s1 + 1, s2))
+			return (wildcmp(s1, s2 + 1));
 	}
-	if (*s1 != *s2) /* regular character mismatch */
-		return (0);
-	return (wildcmp(s1 + 1, s2 + 1)); /* both characters match */
+	else if (*s1 != '\0')
+	{
+		if (*s1 == *s2)
+			return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	return (0);
 }
