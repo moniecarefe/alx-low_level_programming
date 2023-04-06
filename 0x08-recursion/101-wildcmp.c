ee9ae7c2b921b652bcf88f638a6f83c837475659
Xymdia <miteldream@gmail.com>
2023-04-06 09:26:04 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,32 @@
+#include "main.h"
+
 /**
- * wildcmp - compare two strings, considering that s2 may contain wildcard *
- * @s1: the first string
- * @s2: the second string, may contain wildcard *
+ * wildcmp - Compares two strings, allowing for wildcard characters.
+ * @s1: The string to compare.
+ * @s2: The string that may contain wildcard characters.
  *
- * Return: 1 if the strings can be considered identical, otherwise return 0.
+ * Return: If the strings can be considered identical - 1.
+ *         Otherwise - 0.
  */
 int wildcmp(char *s1, char *s2)
 {
+    if (*s1 == '\0' && *s2 == '\0')
+        return (1);
+
+    if (*s1 == *s2)
+        return (wildcmp(s1 + 1, s2 + 1));
+
     if (*s2 == '*')
     {
         if (*(s2 + 1) == '\0')
             return (1);
-        else if (*s1 == '\0')
+
+        if (*s1 == '\0' && *(s2 + 1) == '*')
             return (0);
-        else
-            return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-    }
-    else if (*s1 == *s2)
-    {
-        if (*s1 == '\0')
+
+        if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
             return (1);
-        else
-            return (wildcmp(s1 + 1, s2 + 1));
     }
-    else
-        return (0);
+
+    return (0);
 }
