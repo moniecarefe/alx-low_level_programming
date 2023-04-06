c646263f5d7cd1446dc3445bde2e0e137524c5b5
Xymdia <miteldream@gmail.com>
2023-04-06 09:06:36 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,11 @@
-#include "main.h"
+/**
+ * is_prime_helper - checks if a number is prime
+ * @n: the number to check
+ * @i: the divisor to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_helper(int n, int i);
 
 /**
  * is_prime_number - checks if a number is prime
@@ -6,27 +13,28 @@
  *
  * Return: 1 if n is prime, 0 otherwise
  */
-int is_prime_helper(int n, int i)
-{
-	if (n < 2 || (n != 2 && n % 2 == 0))
-	{
-		return (0);
-	}
-	else if (i > n / 2)
-	{
-		return (1);
-	}
-	else if (n % i == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (is_prime_helper(n, i + 2));
-	}
-}
-
 int is_prime_number(int n)
 {
-	return (is_prime_helper(n, 3));
+    if (n <= 1)
+        return (0);
+
+    return (is_prime_helper(n, 2));
+}
+
+/**
+ * is_prime_helper - checks if a number is prime
+ * @n: the number to check
+ * @i: the divisor to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_helper(int n, int i)
+{
+    if (i == n)
+        return (1);
+
+    if (n % i == 0)
+        return (0);
+
+    return (is_prime_helper(n, i + 1));
 }
