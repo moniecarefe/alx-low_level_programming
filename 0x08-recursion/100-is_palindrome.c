e2f9848a497ce2e65a0bbff55efaaa80230e1e25
Xymdia <miteldream@gmail.com>
2023-04-06 09:15:58 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,51 +2,47 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string to check the length
+ * @s: the string to measure
  *
- * Return: length of the string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-    int len = 0;
-
-    while (*s++)
-        len++;
-
-    return (len);
+    if (*s == '\0')
+        return (0);
+    else
+        return (1 + _strlen(s + 1));
 }
 
 /**
- * _palindrome_helper - checks if a string is palindrome
- * @s: string to check
- * @i: left index
- * @j: right index
+ * _palindrome_helper - checks if a string is a palindrome
+ * @s: the string to check
+ * @i: the current index
+ * @j: the current index from the end of the string
  *
- * Return: 1 if palindrome, 0 otherwise
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int _palindrome_helper(char *s, int i, int j)
 {
     if (i >= j)
         return (1);
-
-    if (s[i] != s[j])
+    else if (s[i] != s[j])
         return (0);
-
-    return (_palindrome_helper(s, i + 1, j - 1));
+    else
+        return (_palindrome_helper(s, i + 1, j - 1));
 }
 
 /**
- * is_palindrome - checks if a string is palindrome
- * @s: string to check
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string to check
  *
- * Return: 1 if palindrome, 0 otherwise
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
     int len = _strlen(s);
-
-    if (len == 0 || len == 1)
+    if (len <= 1)
         return (1);
-
-    return (_palindrome_helper(s, 0, len - 1));
+    else
+        return (_palindrome_helper(s, 0, len - 1));
 }
