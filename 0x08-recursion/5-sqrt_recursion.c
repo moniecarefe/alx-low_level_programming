516840e3766cb76fdc9b5352879bbd6562d887d1
Xymdia <miteldream@gmail.com>
2023-04-06 09:01:30 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,33 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
- * @n: The number to find the square root of
+ * find_sqrt - finds the square root of a number
+ * @n: the number to find the square root of
+ * @i: the current guess
  *
- * Return: The natural square root of n, or -1 if n does not have a natural square root
+ * Return: the square root if found, -1 otherwise
+ */
+int find_sqrt(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (find_sqrt(n, i + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the square root if found, -1 otherwise
  */
 int _sqrt_recursion(int n)
 {
@@ -12,45 +35,8 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
 	else
 	{
-		return (find_sqrt(n, 1, n));
-	}
-}
-
-/**
- * find_sqrt - Finds the natural square root of a number
- * @n: The number to find the square root of
- * @start: The starting value for the search
- * @end: The ending value for the search
- *
- * Return: The natural square root of n, or -1 if n does not have a natural square root
- */
-int find_sqrt(int n, int start, int end)
-{
-	int mid = (start + end) / 2;
-
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid < n)
-	{
-		if ((mid + 1) * (mid + 1) > n)
-		{
-			return (mid);
-		}
-		else
-		{
-			return (find_sqrt(n, mid + 1, end));
-		}
-	}
-	else
-	{
-		return (find_sqrt(n, start, mid - 1));
+		return (find_sqrt(n, 1));
 	}
 }
