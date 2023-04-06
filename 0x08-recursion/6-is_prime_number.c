edb25d3c3eef731f356f4a036b39350d04464183
Xymdia <miteldream@gmail.com>
2023-04-06 09:10:46 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,38 @@
 #include "main.h"
 
 /**
- * is_prime_number - Checks if a number is prime
- * @n: Number to check
+ * is_prime_helper - checks if a number is prime recursively
+ * @n: the number to check
+ * @i: the divisor to check n with
  *
- * Return: 1 if n is prime, 0 otherwise
+ * Return: 1 if prime, 0 otherwise
  */
-int is_prime_number(int n)
+int is_prime_helper(int n, int i)
 {
 	if (n <= 1)
 		return (0);
-
-	return (_is_prime_helper(n, 2));
+	else if (n == 2 || n == 3)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	else if (i * i > n)
+		return (1);
+	else
+		return (is_prime_helper(n, i + 2));
 }
 
 /**
- * _is_prime_helper - Recursive helper function for is_prime_number
- * @n: Number to check
- * @i: Divisor to try
+ * is_prime_number - checks if a number is prime
+ * @n: the number to check
  *
- * Return: 1 if n is prime, 0 otherwise
+ * Return: 1 if prime, 0 otherwise
  */
-int _is_prime_helper(int n, int i)
+int is_prime_number(int n)
 {
-	if (i == n)
+	if (n == 2 || n == 3)
 		return (1);
-
-	if (n % i == 0)
+	else if (n <= 1 || n % 2 == 0)
 		return (0);
-
-	return (_is_prime_helper(n, i + 1));
+	else
+		return (is_prime_helper(n, 3));
 }
