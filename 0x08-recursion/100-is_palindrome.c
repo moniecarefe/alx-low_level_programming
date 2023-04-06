3ce7f76fa354515ea9bc938829f1c279aebe91d5
Xymdia <miteldream@gmail.com>
2023-04-06 09:23:39 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,41 +1,48 @@
 #include "main.h"
-
 /**
- * str_len - returns the length of a string
- * @str: the string to get the length of
+ * _strlen_recursion - Returns the length of a string
+ * @s: Pointer to the string
  *
- * Return: the length of the string
+ * Return: Length of the string
  */
-int str_len(char *str)
+int _strlen_recursion(char *s)
 {
-    int len = 0;
-
-    while (*(str++))
-        len++;
-
-    return (len);
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
 }
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
+ * is_palindrome_helper - Helper function for is_palindrome
+ * @s: Pointer to the string
+ * @start: Starting index of the string
+ * @end: Ending index of the string
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: Pointer to the string
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-    int len = str_len(s);
-    char *start = s;
-    char *end = s + len - 1;
+	int len = _strlen_recursion(s);
 
-    while (start < end)
-    {
-        if (*start != *end)
-            return (0);
-
-        start++;
-        end--;
-    }
-
-    return (1);
+	if (len <= 1)
+		return (1);
+	else
+		return (is_palindrome_helper(s, 0, len - 1));
 }
