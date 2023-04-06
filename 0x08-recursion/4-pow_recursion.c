029129a8526684b05bfa00416a87e9f78d6b8ad2
Xymdia <miteldream@gmail.com>
2023-04-06 08:58:00 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,24 +1,24 @@
-/**
- * _pow_recursion - Computes the value of x raised to the power of y
- * @x: The base number
- * @y: The exponent
- *
- * Return: The result of x raised to the power of y
- */
+#include "main.h"
 
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y
+ * @x: The base value
+ * @y: The exponent value
+ *
+ * Return: The result of x raised to the power of y, or -1 if y is less than 0
+ */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return -1;
-    if (y == 0)
-        return 1;
-    if (y == 1)
-        return x;
-
-    int res = _pow_recursion(x, y / 2);
-
-    if (y % 2 == 0)
-        return res * res;
-    else
-        return x * res * res;
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
