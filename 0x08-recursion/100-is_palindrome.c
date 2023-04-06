31e66f1199166f2b45667319444698a5953d7ab6
Xymdia <miteldream@gmail.com>
2023-04-06 09:14:18 +0100

    0x08. C - Recursion for ALX SE

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,52 +1,52 @@
 #include "main.h"
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
+ * _strlen - returns the length of a string
+ * @s: string to check the length
  *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (*s++)
+        len++;
+
+    return (len);
+}
+
+/**
+ * _palindrome_helper - checks if a string is palindrome
+ * @s: string to check
+ * @i: left index
+ * @j: right index
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int _palindrome_helper(char *s, int i, int j)
+{
+    if (i >= j)
+        return (1);
+
+    if (s[i] != s[j])
+        return (0);
+
+    return (_palindrome_helper(s, i + 1, j - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is palindrome
+ * @s: string to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-    int len;
+    int len = _strlen(s);
 
-    len = _strlen_recursion(s);
-
-    if (len <= 1)
+    if (len == 0 || len == 1)
         return (1);
 
     return (_palindrome_helper(s, 0, len - 1));
-}
-
-/**
- * _palindrome_helper - recursively checks if a string is a palindrome
- * @s: the string to check
- * @start: the starting index to compare
- * @end: the ending index to compare
- *
- * Return: 1 if s is a palindrome, 0 otherwise
- */
-int _palindrome_helper(char *s, int start, int end)
-{
-    if (start >= end)
-        return (1);
-
-    if (s[start] != s[end])
-        return (0);
-
-    return (_palindrome_helper(s, start + 1, end - 1));
-}
-
-/**
- * _strlen_recursion - returns the length of a string
- * @s: the string to check
- *
- * Return: the length of s
- */
-int _strlen_recursion(char *s)
-{
-    if (*s == '\0')
-        return (0);
-
-    return (1 + _strlen_recursion(s + 1));
 }
